059231f7a060caf13efd129fea06a26cd7a7b08b
Twinparadox <nww731@gmail.com>
2017-12-03 21:53:51 +0900

    백준 알고리즘 문제

---
+++ Baekjoon/11053.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int max, n, arr[1001] = { 0, }, dp[1001] = { 0, };
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> arr[i];
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j < i; j++)
+			if (arr[j] < arr[i])
+				dp[i] = dp[i] < (dp[j] + 1) ? (dp[j] + 1) : dp[i];
+	max = -1;
+	for (int i = 1; i <= n; i++)
+		if (max < dp[i])
+			max = dp[i];
+	cout << max + 1;
+}
