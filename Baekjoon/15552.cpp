8da412e74e732a3dec1516548f869f63959923f6
Twinparadox <nww731@gmail.com>
2018-04-01 11:05:13 +0900

    백준 알고리즘 문제 풀이

---
+++ Baekjoon/15552.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int t, a, b;
+	cin >> t;
+	while (t--)
+	{
+		cin >> a >> b;
+		cout << a + b << '\n';
+	}
+}
