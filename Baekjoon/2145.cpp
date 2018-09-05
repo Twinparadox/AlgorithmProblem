b316ed61b6d997c29170116db3339e39bd091e18
Wonwoo Nam <nww731@gmail.com>
2018-09-05 23:00:00 +0900

    BOJ 문제 풀이

---
+++ Baekjoon/2145.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int n, tmp1, tmp2;
+	while (1)
+	{
+		cin >> n;
+		if (n == 0)
+			break;
+		
+		while (n >= 10)
+		{
+			tmp1 = n, tmp2 = 0;
+			while (tmp1 > 0)
+			{
+				tmp2 += tmp1 % 10;
+				tmp1 /= 10;
+			}
+			n = tmp2;
+		}
+		cout << n << '\n';
+	}
+}
