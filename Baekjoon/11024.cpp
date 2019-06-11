a77bea4df10acbc02fbed265d07d1c6494c8144e
WonWoo Nam <nww731@gmail.com>
2019-06-11 19:04:29 +0900

    BOJ 문제 재채점 제출

--- Baekjoon/11024.cpp
+++ Baekjoon/11024.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
+void tokenizer(string& s, vector<int>& token, string delimiter)
+{
+	size_t endPos = s.find_first_not_of(delimiter, 0);
+	size_t startPos = s.find_first_of(delimiter, endPos);
+	while (string::npos != startPos || string::npos != endPos)
+	{
+		token.push_back(stoi(s.substr(endPos, startPos - endPos)));
+		endPos = s.find_first_not_of(delimiter, startPos);
+		startPos = s.find_first_of(delimiter, endPos);
+	}
+}
 int main(void)
 {
-	int t, sum, len;
+	int t, sum, len, size;
 	cin >> t;
 	cin.ignore();
 	while (t--)
 	{
-		string s;
+		string s, delimiter = " ";
+		vector<int> arr;
 		getline(cin, s);
-		len = s.length() , sum = 0;
-		for (int i = 0; i < len; i += 2)
-			sum += (int)s[i] - '0';
+
+		tokenizer(s, arr, delimiter);
+		size = arr.size();
+		sum = 0;
+
+		for (int i = 0; i < size; i++)
+			sum += arr[i];
 		cout << sum << '\n';
 	}
 }
