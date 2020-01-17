60043ccfe35b1e2e36c57a5bae2588606cf2ed62
WonWoo Nam <nww731@gmail.com>
2020-01-17 20:42:16 +0900

    BOJ 문제 풀이

---
+++ Baekjoon/1927.cpp
@@ -0,0 +1,31 @@
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+int main(void)
+{
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(false);
+	int N, x;
+	priority_queue<int, vector<int>, greater<int> > pq;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> x;
+		if (x)
+			pq.push(x);
+		else
+		{
+			if (pq.empty())
+				cout << "0\n";
+			else
+			{
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+	}
+}
